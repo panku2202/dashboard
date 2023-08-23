b057a6fef09c482cb898fc2f0d8796ec771e0a41
Pankaj Kumar <pankaj.kumar@corp.litmusworld.com>
2023-08-23 10:03:59 +0530

    first with side drawer

---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent implements OnInit {
+
+  list=[
+    {
+      number:'1',
+      name:'home',
+      icon:'fa-solid fa-house',
+    },
+
+    {
+      number:'2',
+      name:'Analytics',
+      icon:'fa-solid fa-chart-line',
+    },
+    {
+      number:'3',
+      name:'Product',
+      icon:'fa-solid fa-box',
+    },
+    {
+      number:'4',
+      name:'Order',
+      icon:'fa-solid fa-cart-shopping',
+    },
+    {
+      number:'5',
+      name:'Settings',
+      icon:'fa-solid fa-gear',
+    },
+    {
+      number:'6',
+      name:'About',
+      icon:'fa-solid fa-circle-info',
+    },
+    {
+      number:'7',
+      name:'Contact',
+      icon:'fa-solid fa-phone',
+    },
+
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

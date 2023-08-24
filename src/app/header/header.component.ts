052fca8f74bf62ef670f6bf77367448c7a876883
Pankaj Kumar <pankaj.kumar@corp.litmusworld.com>
2023-08-24 10:28:25 +0530

    side drawer toggle

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+selector: 'app-header',
+templateUrl: './header.component.html',
+styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+@Output() sideNavToggled = new EventEmitter<boolean>();
+menuStatus: boolean = false;
+constructor() { }
 
-  constructor() { }
+ngOnInit(): void {
+}
 
-  ngOnInit(): void {
-  }
-
-  
-
+toggleSideNav() {
+    this.menuStatus = !this.menuStatus;
+    this.sideNavToggled.emit(this.menuStatus); // You can emit 'true' or 'false' based on your use case
+}
 }
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,54 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+selector: 'app-sidenav',
+templateUrl: './sidenav.component.html',
+styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
-
-  list=[
+@Input() sideNavStatus: boolean = false;
+list=[
     {
-      number:'1',
-      name:'home',
-      icon:'fa-solid fa-house',
+     number:'1',
+     name:'home',
+     icon:'fa-solid fa-house',
     },
 
     {
-      number:'2',
-      name:'Analytics',
-      icon:'fa-solid fa-chart-line',
+     number:'2',
+     name:'Analytics',
+     icon:'fa-solid fa-chart-line',
     },
     {
-      number:'3',
-      name:'Product',
-      icon:'fa-solid fa-box',
+     number:'3',
+     name:'Product',
+     icon:'fa-solid fa-box',
     },
     {
-      number:'4',
-      name:'Order',
-      icon:'fa-solid fa-cart-shopping',
+     number:'4',
+     name:'Order',
+     icon:'fa-solid fa-cart-shopping',
     },
     {
-      number:'5',
-      name:'Settings',
-      icon:'fa-solid fa-gear',
+     number:'5',
+     name:'Settings',
+     icon:'fa-solid fa-gear',
     },
     {
-      number:'6',
-      name:'About',
-      icon:'fa-solid fa-circle-info',
+     number:'6',
+     name:'About',
+     icon:'fa-solid fa-circle-info',
     },
     {
-      number:'7',
-      name:'Contact',
-      icon:'fa-solid fa-phone',
+     number:'7',
+     name:'Contact',
+     icon:'fa-solid fa-phone',
     },
 
-  ]
-  constructor() { }
+]
+constructor() { }
 
-  ngOnInit(): void {
-  }
+ngOnInit(): void {
+}
 
 }
